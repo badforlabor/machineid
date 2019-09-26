468b633b49340de8732a8d0aeca3305c6daedb47
liubo04 <liubo04@work.com>
2019-09-26 12:05:04 +0800

    完美

---
+++ machineid/main.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+#include "machineid/machineid.h"
+
+int main()
+{
+	extern std::string md5(std::string dat);
+
+	std::cout << md5(machineid::machineHash()) << std::endl;
+	system("pause");
+
+	return 0;
+}
